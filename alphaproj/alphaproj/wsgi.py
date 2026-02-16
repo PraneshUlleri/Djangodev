fa596db5b6df98e874c6f41132a0d3da8ab5db62
PraneshUlleri <ipraneshulleri@gmail.com>
2026-02-16 23:36:10 +0530

    ran the cal app

--- alphaproj/alphaproj/wsgi.py
+++ alphaproj/alphaproj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for trial project.
+WSGI config for alphaproj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alphaproj.settings')
 
 application = get_wsgi_application()
